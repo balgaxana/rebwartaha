af6ab820801a8dac58ec6ad7cdb7809c9fc6c6f6
balgaxana <phishingtechnique@gmail.com>
2025-01-21 21:41:14 +0300

    Update scr.js

--- scr.js
+++ scr.js
@@ -1,6 +1,9 @@
 // Replace the URL after a delay
-window.onload = function() {
+window.addEventListener('load', function() {
     setTimeout(function() {
-        document.getElementById('myIframe').src = 'https://www.youtube.com/watch?v=aPiKLan6Ai4&list=RDaPiKLan6Ai4&start_radio=1';
+        var iframe = document.getElementById('myIframe');
+        if (iframe) {
+            iframe.src = 'https://www.youtube.com/watch?v=aPiKLan6Ai4&list=RDaPiKLan6Ai4&start_radio=1';
+        }
     }, 1000); // Adjust the delay as needed
-};
+});
