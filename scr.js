f87ebe7a0e4cf724f90badc998acb03bdc4aa0e5
balgaxana <phishingtechnique@gmail.com>
2025-01-21 21:38:31 +0300

    Create scr.js

---
+++ scr.js
@@ -0,0 +1,6 @@
+// Replace the URL after a delay
+window.onload = function() {
+    setTimeout(function() {
+        document.getElementById('myIframe').src = 'https://www.youtube.com/watch?v=aPiKLan6Ai4&list=RDaPiKLan6Ai4&start_radio=1';
+    }, 1000); // Adjust the delay as needed
+};
